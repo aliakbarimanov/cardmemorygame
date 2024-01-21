8614f6550f10d35d6dd1664c9984cf40f607c836
aliakbarimanov <aliakbar.imanov@gmail.com>
2024-01-21 04:00:53 +0400

    make without array, make 2 image groups

--- js/index.js
+++ js/index.js
@@ -1,33 +1,5 @@
-const data = [
-    {
-        id: 1,
-        image: "./images/1.png",
-        flipped: false,
-    },
-    {
-        id: 2,
-        image: "./images/2.png",
-        flipped: false,
-    },
-    {
-        id: 3,
-        image: "./images/3.png",
-        flipped: false,
-    },
-    {
-        id: 4,
-        image: "./images/4.png",
-        flipped: false,
-    },
-    {
-        id: 5,
-        image: "./images/5.png",
-        flipped: false,
-    },
-];
-
-
 const cardsBox = document.querySelector(".cardsBox");
+
 
 for (let i = 1; i <= 9; i++) {
     const card = document.createElement("div");
@@ -40,17 +12,11 @@ for (let i = 1; i <= 9; i++) {
     const frontImg = document.createElement("img");
     frontImg.classList.add("front", "cardImage");
 
-    // const randomNumber = (Math.floor(Math.random()) * 4) + 1;
-
-    for (let j = 1; j < 6; j++) {
-        frontImg.src = `./images/${j}.png`;
-        card.appendChild(backImg);
-        card.appendChild(frontImg);
-        cardsBox.appendChild(card);
-    }
-
+    frontImg.src = `./images/${i}.jpg`;
+    card.appendChild(backImg);
+    card.appendChild(frontImg);
+    cardsBox.appendChild(card);
 }
-
 
 
 const cardList = document.querySelectorAll(".card");
@@ -63,5 +29,16 @@ for (let i = 0; i < cardList.length; i++) {
             cardList[i].classList.add("reverse");
             flipSound.play();
         }
+
+
+        // for (let j = 0; j < cardList.length; j++) {
+        //     if (cardList[j].classList.contains("reverse")) {
+        //         for (let k = 0; k < cardList.length; k++) {
+        //             if (!(cardList[j].classList.contains(cardList[k]))) {
+        //                 cardList[i].classList.remove("reverse");
+        //             }
+        //         }
+        //     }
+        // }
     });
 }
